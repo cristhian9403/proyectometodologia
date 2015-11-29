d9f6dab2a1c94f5ba29267b63c11f0d35dbd1246
cristhian9403 <cristhian.camilo.alvarez@correounivalle.edu.co>
2015-11-28 22:04:07 -0500

    buscardor final
    si se agregan video o cosas asi ps creo q se deberia hacer un
    direccionamiento a ellos

--- buscador/db.js
+++ buscador/db.js
@@ -2,6 +2,10 @@
 
 /* configuration settings */
 
+'<meta charset="utf-8">'
+'<meta http-equiv="X-UA-Compatible" content="IE=edge">'
+'<meta name="viewport" content="width=device-width, initial-scale=1">'
+
 	searchname = 'buscar.html'
 	
 	usebannercode=true
@@ -9,11 +13,11 @@ ButtonCode = "<button type='submit' class='btn btn-primary'>Buscar</button>"
 	function templateBody() {
 		document.write('<'+
 		'script language="Javascript">'+
-		'<'+'/'+'script'+'></head><body bgcolor="#ffffff" text="#000000" link="#000099" vlink="#996699" alink="#996699"><table border=0 width=100%><tr><td>');
+		'<'+'/'+'script'+'><table border=0 width=100%><tr><td>');
 	}
 
 	function templateEnd() {
-		document.write('</td></tr></table></font></center></body></html>');
+		document.write('</td></tr></table>');
 	}
 	function bannerCode() {
 	}	
@@ -22,39 +26,16 @@ ButtonCode = "<button type='submit' class='btn btn-primary'>Buscar</button>"
 
 /* database records */
 
-add("<a href='http://www.google.com'>Programa para resolver problemas de matemáticas 2011</a>","prueba","esta es la pagina de gogle")
-add("<a href='http://www.websoftech.tk'>WebSoftech</a>","softech","Crea aplicaciones para la web, sitios web con panel administrativo, animaciones en flash, bases de datos, TODO referente ala web")
-add("<a href='http://www.facebook.com'>FACEBOOK</a>","facebook red social","UNA DE LAS MEJORES REDES SOCIALES ACTUALMENTE EN EL MUNDO")
-add("<a href='http://www.taringa.net'>TARINGA</a>","taringa","RED SOCIAL EN DONDE ENCONTRAMOS DIVERSAS OPCIONES PARA NUESTRAS INQUIETUDES O DUDAS SOBRE TODO!!")
-add("<a href='http://poringa.net'>PORINGA</a>","sexo porno sex amateur","porno colectivo!")
-add("<a href='http://hotmail.com'>HOTMAIL</a>","hotmail correo electronico","correo electronico")
-add("<a href='www.pixestv.tk'>pixesTV</a>","pixesTV, tv en vivo","TV en vivo, podras vet tv peruana y del mundo")
-add("<a href='http://www.pixestv.tk/'>PixesTV</a>","pixesTV tv en vivo tv peruana en vivo","PixesTV podras ver television peruana y del mundo, ademas encontraras las ultimas noticias de la farandula")
-add("<a href='http://www.areshelp.com'>sbot 1.3 by grenman </a>","sb0t , sbto","Some stupid people Ares Have created a program called \"Overflow\" which exploits a vulnerability that had sb0t For cause \"buffer overflow\" Making us the following appears:")
-add("<a href='http://www.kogimobile.com'>kogiMobile</a>","kogimobile android html5 java javascript jquery","Empresa de desarrollo de software")
-add("<a href='http://www.youtube.com'>Youtube.com</a>","videos","alojamiento de videos web gratis.")
-add("<a href='http://leonardopc2100.jimdo.com'>Inicio</a>","Inicio","Inicio de la pagina")
-add("<a href='http://leonardopc2100.jimdo.com'>Inicio</a>","Inicio","Inicio de la pagina")
-add("<a href='000webhost.com'>000webhost</a>","000webhost","crea tu propia pagina web")
-add("<a href='www.dragonballesp.jimdo.com'>Dragon Ball ESP</a>","dragon,ball,esp","Comunidad de Dragon Ball en Español")
-add("<a href='http://dragonballesp.jimdo.com/'>Dragon Ball ESP</a>","dragon,ball,esp","Comunidad de Dragon Ball en Español")
-add("<a href='http://directoriotelefonicodeyumbo.es.tl/%3Cfont-color%3D-g-yellow-g-%3ERestaurante-Davinchi.htm'>Restaurante Davinchi</a>","Restaurante Davinchi","La mejor comida")
-add("<a href='http://allejandro92.es.tl/'>Allejandro92</a>","allejandro","Canal de youtube")
-add("<a href='http://wallpaperscolon.com.ar/dragon_ball_z_cell_game.htm'>dragon_ball</a>","imagen wallpapers","buena imagen anime")
-add("<a href='http://wallpaperscolon.com.ar/dragon_ball_z_cell_game.htm'>cell_game</a>","cell_game wallpapers","cell game wallpapers")
-add("<a href='http://wallpaperscolon.com.ar/dragon_ball_z_cell_game.htm'>cellgame</a>","wallpapers de cell game","wallpapers de cell game dragon")
-add("<a href='http://denturuguay.jimdo.com'>DentUruguay</a>","denturuguay productos","Artículos dentales")
-add("<a href='http://denturuguay.jimdo.com'>denturuguay</a>","articulos dentales uruguay","ARTICULOS DENTALES URUGUAY")
-add("<a href='http://www.denturuguay.jimdo.com'>denturuguay</a>","denturuguay","productos dentales uruguay uniformes de trabajo")
-add("<a href='file:///C:/Users/sandra/Desktop/muvich/muvich.com_Inicio.html'>muvich</a>","muvich","muvich")
-add("<a href='file:///C:/Users/sandra/Desktop/muvich/muvich.com_Inicio.html'>muvich</a>","muvich","muvich")
-add("<a href='OrangeCorp'>OrangeCrop</a>","OrangeCrop","Es una corporacion de informacion")
-add("<a href='http://www.facebook.com/'>FACEBOOK</a>","facebook una red social","ES UNA DE LAS MEJORES REDES SOCIALES EN EL MUNDO")
-add("<a href='http://www.youtube.com/'>YouTube broadcast yourself</a>","youtube broadcast yourself  reproductor ","Uno de los mejores reproductores de video en el mundo ya que cuenta con videos de alta calidad formato HD")
-add("<a href='http://www.google.com/'>GOOGLE</a>","google buscador","ES UNO DE LOS MEJORES BUSCADORES A NIVEL MUNDIAL")
-add("<a href='http://www.wix.com/clanfla/swat4'>FLA</a>","fuerza latinos americanos","CLAN ONLINE ABIERTO,,AYUDA CONSULTAR DUDAS CON zerafin,dj,kratos,boss,yorluis y chuy")
-add("<a href='http://www.wix.com/clanfla/swat4'>FUERZA LATINOS AMERICANOS</a>","fuerza latinos americanos","CLAN ONLINE ABIERTO,,AYUDA CONSULTAR DUDAS CON zerafin,dj,kratos,boss,yorluis y chuy")
-add("<a href=''></a>","","")
-add("<a href='iphone-jailbreak-noticias.html'>iPhone JB Notticias</a>","iphone jailbreak noticias .com","iPhone JB noticias la mejor Web de JB!")
-add("<a href='iphone-jailbreak-noticias.html'>iPhone JB Notticias</a>","iphone jailbreak noticias .com","hors")
-add("<a href='www.delosinfo.es'>delos</a>","delos","delos")
+add("<a href='../vistas/Psicologia/Definicion.html'>Definición de la Psicología</a>","Definicion","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Historia.html'>Historia de la psicología</a>","Fundamentos Históricos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Biologicos.html'>Temas de Biologia</a>","Fundamentos Biológicos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Filosoficos.html'>Filosofia</a>","Fundamentos Filosóficos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Psicologia/Glosario.html'>Glosario</a>","Glosario","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Social.html'>Psicología Social</a>","Social","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Educativa.html'>Psicología Educativa</a>","Educativa","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Clinica.html'>Psicología Clínica</a>","Clínica","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Ramas/Organizacional.html'>Psicología Organizacional</a>","Organizacional","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Eventos.html'>Eventos</a>","Eventos","esta es la pagina de Psicología a tu alcance")
+add("<a href='../vistas/Presentacion.html'>Presentación</a>","esta es la pagina de Psicología a tu alcance")
+add("<a href='http://leonardopc2100.jimdo.com'>Inicio</a>","Inicio","esta es la pagina de Psicología a tu alcance")
+add("<a href='http://leonardopc2100.jimdo.com'>Inicio</a>","Inicio","esta es la pagina de Psicología a tu alcance")
